14901bd5d246cbfca9942d2a2156b51de554e338
Arif Mandal <arifmandal96@gmail.com>
2024-01-24 19:40:37 +0300

    Added connect and disconnect button.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,21 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.cbComPort = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.btnDisconnect);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.cbComPort);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(24, 36);
@@ -44,6 +52,15 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Bluetooth Connection";
+            // 
+            // cbComPort
+            // 
+            this.cbComPort.FormattingEnabled = true;
+            this.cbComPort.Location = new System.Drawing.Point(99, 33);
+            this.cbComPort.Name = "cbComPort";
+            this.cbComPort.Size = new System.Drawing.Size(180, 28);
+            this.cbComPort.TabIndex = 1;
+            this.cbComPort.Text = "COM1";
             // 
             // label1
             // 
@@ -54,14 +71,41 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "COM Port: ";
             // 
-            // cbComPort
+            // label2
             // 
-            this.cbComPort.FormattingEnabled = true;
-            this.cbComPort.Location = new System.Drawing.Point(99, 33);
-            this.cbComPort.Name = "cbComPort";
-            this.cbComPort.Size = new System.Drawing.Size(180, 28);
-            this.cbComPort.TabIndex = 1;
-            this.cbComPort.Text = "COM1";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Baudrate:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(99, 86);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 28);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "38400";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(11, 149);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(107, 42);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(11, 213);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(107, 42);
+            this.btnDisconnect.TabIndex = 5;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -82,6 +126,10 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.ComboBox cbComPort;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
